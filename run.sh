f2c5833672181dfeff9b17fde6283540c0ebdf61
Michał Dyczko <dykomch@gmail.com>
2021-07-13 14:13:13 +0200

    Typo

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@ module av
 if [ ! -e roberta_large_transformers ] ; then
     mkdir roberta_large_transformers &&  wget -q -O tmp.zip https://github.com/sdadas/polish-roberta/releases/download/models-transformers-v3.4.0/roberta_large_transformers.zip && unzip tmp.zip -d roberta_large_transformers && rm tmp.zip
 fi
-python src/train.py --cuda=True --pretrained-model=roberta_large_transformers --freeze-bert=False --lstm-dim=-1 --language=polish --seed=1 --lr=5e-6 --epoch=20 --use-crf=False --augment-type=all  --augment-rate=0.1 --alpha-sub=0.4 --alpha-del=0.4 --data-path=data --save-path=out_roberta_large_transformers
+python3 src/train.py --cuda=True --pretrained-model=roberta_large_transformers --freeze-bert=False --lstm-dim=-1 --language=polish --seed=1 --lr=5e-6 --epoch=20 --use-crf=False --augment-type=all  --augment-rate=0.1 --alpha-sub=0.4 --alpha-del=0.4 --data-path=data --save-path=out_roberta_large_transformers
