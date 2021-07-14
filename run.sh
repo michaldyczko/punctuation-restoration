7b71ff7663881c6fec10919f15a82304f52fc3cc
Michał Dyczko <dykomch@gmail.com>
2021-07-14 14:23:20 +0200

    Prepare for multi-gpu training

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH -N 1
-#SBATCH -n 4
-#SBATCH -c 4
+#SBATCH -n 1
+#SBATCH -c 16
 #SBATCH -A g85-964
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --exclude=rysy-n7
 #SBATCH --time=10:00:00
 #SBATCH -o out.%j.out
@@ -19,4 +19,4 @@ else
     echo "----------- roberta_large_transformers FOUND -----------"
 fi
 echo "----------- TRAINING -----------"
-python3 src/train.py --cuda=True --pretrained-model=roberta_large_transformers --freeze-bert=False --lstm-dim=-1 --language=polish --seed=1 --lr=5e-6 --epoch=20 --use-crf=False --augment-type=all  --augment-rate=0.1 --alpha-sub=0.4 --alpha-del=0.4 --data-path=data --save-path=out_roberta_large_transformers
+python3 src/train.py --cuda=True --pretrained-model=roberta_large_transformers --freeze-bert=False --lstm-dim=-1 --language=polish --seed=1 --lr=5e-6 --epoch=50 --use-crf=False --augment-type=all  --augment-rate=0.1 --alpha-sub=0.4 --alpha-del=0.4 --data-path=data --save-path=test_mgpu --batch-size=128
