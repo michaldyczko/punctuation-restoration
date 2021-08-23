77930f31b7ec44fc05497a4f492b29cdc9d41c79
Michał Dyczko <dykomch@gmail.com>
2021-08-23 10:42:38 +0200

    Changed inference

--- src/inference.py
+++ src/inference.py
@@ -79,66 +79,68 @@ def inference():
     deep_punctuation.eval()
 
     with open(args.in_file, 'r', encoding='utf-8') as f:
-        text = f.read()
-    text = re.sub(r"[,:\-–.!;?]", '', text)
-    words_original_case = text.split()
-    words = text.lower().split()
-
-    word_pos = 0
-    sequence_len = args.sequence_length
+        bigtext = f.read()
     result = ""
-    decode_idx = 0
+    for text in bigtext.split('\n'):
+        text = re.sub(r"[,:\-–.!;?]", '', text)
+        words_original_case = text.split()
+        words = text.lower().split()
 
-    # punctuation_map = {0: '', 1: ',', 2: '.', 3: '?'}
-    if args.language == 'bn':
-        punctuation_map[4] = '।'
+        word_pos = 0
+        sequence_len = args.sequence_length
+        decode_idx = 0
 
-    while word_pos < len(words):
-        x = [TOKEN_IDX[token_style]['START_SEQ']]
-        y_mask = [0]
+        # punctuation_map = {0: '', 1: ',', 2: '.', 3: '?'}
+        if args.language == 'bn':
+            punctuation_map[4] = '।'
 
-        while len(x) < sequence_len and word_pos < len(words):
-            tokens = tokenizer.tokenize(words[word_pos])
-            if len(tokens) + len(x) >= sequence_len:
-                break
-            else:
-                for i in range(len(tokens) - 1):
-                    x.append(tokenizer.convert_tokens_to_ids(tokens[i]))
-                    y_mask.append(0)
-                x.append(tokenizer.convert_tokens_to_ids(tokens[-1]))
-                y_mask.append(1)
-                word_pos += 1
-        x.append(TOKEN_IDX[token_style]['END_SEQ'])
-        y_mask.append(0)
-        if len(x) < sequence_len:
-            x = x + [
-                TOKEN_IDX[token_style]['PAD'] for _ in range(sequence_len - len(x))
-            ]
-            y_mask = y_mask + [0 for _ in range(sequence_len - len(y_mask))]
-        attn_mask = [1 if token != TOKEN_IDX[token_style]['PAD'] else 0 for token in x]
+        while word_pos < len(words):
+            x = [TOKEN_IDX[token_style]['START_SEQ']]
+            y_mask = [0]
 
-        x = torch.tensor(x).reshape(1, -1)
-        y_mask = torch.tensor(y_mask)
-        attn_mask = torch.tensor(attn_mask).reshape(1, -1)
-        x, attn_mask, y_mask = x.to(device), attn_mask.to(device), y_mask.to(device)
+            while len(x) < sequence_len and word_pos < len(words):
+                tokens = tokenizer.tokenize(words[word_pos])
+                if len(tokens) + len(x) >= sequence_len:
+                    break
+                else:
+                    for i in range(len(tokens) - 1):
+                        x.append(tokenizer.convert_tokens_to_ids(tokens[i]))
+                        y_mask.append(0)
+                    x.append(tokenizer.convert_tokens_to_ids(tokens[-1]))
+                    y_mask.append(1)
+                    word_pos += 1
+            x.append(TOKEN_IDX[token_style]['END_SEQ'])
+            y_mask.append(0)
+            if len(x) < sequence_len:
+                x = x + [
+                    TOKEN_IDX[token_style]['PAD'] for _ in range(sequence_len - len(x))
+                ]
+                y_mask = y_mask + [0 for _ in range(sequence_len - len(y_mask))]
+            attn_mask = [1 if token != TOKEN_IDX[token_style]['PAD'] else 0 for token in x]
 
-        with torch.no_grad():
-            if args.use_crf:
-                y = torch.zeros(x.shape[0])
-                y_predict = deep_punctuation(x, attn_mask, y)
-                y_predict = y_predict.view(-1)
-            else:
-                y_predict = deep_punctuation(x, attn_mask)
-                y_predict = y_predict.view(-1, y_predict.shape[2])
-                y_predict = torch.argmax(y_predict, dim=1).view(-1)
-        for i in range(y_mask.shape[0]):
-            if y_mask[i] == 1:
-                result += (
-                    words_original_case[decode_idx]
-                    + punctuation_map[y_predict[i].item()]
-                    + ' '
-                )
-                decode_idx += 1
+            x = torch.tensor(x).reshape(1, -1)
+            y_mask = torch.tensor(y_mask)
+            attn_mask = torch.tensor(attn_mask).reshape(1, -1)
+            x, attn_mask, y_mask = x.to(device), attn_mask.to(device), y_mask.to(device)
+
+            with torch.no_grad():
+                if args.use_crf:
+                    y = torch.zeros(x.shape[0])
+                    y_predict = deep_punctuation(x, attn_mask, y)
+                    y_predict = y_predict.view(-1)
+                else:
+                    y_predict = deep_punctuation(x, attn_mask)
+                    y_predict = y_predict.view(-1, y_predict.shape[2])
+                    y_predict = torch.argmax(y_predict, dim=1).view(-1)
+            for i in range(y_mask.shape[0]):
+                if y_mask[i] == 1:
+                    result += (
+                        words_original_case[decode_idx]
+                        + punctuation_map[y_predict[i].item()]
+                        + ' '
+                    )
+                    decode_idx += 1
+        result += '\n'
     print('Punctuated text')
     print(result)
     with open(args.out_file, 'w', encoding='utf-8') as f:
